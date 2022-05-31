c5895a2a37e56f542c608ddbe75881e1ee25169a
Daniel Fried <dfried@berkeley.edu>
2022-05-31 13:55:14 -0700

    change assert to warning

--- eval/test_one_solution.py
+++ eval/test_one_solution.py
@@ -28,7 +28,8 @@ def print_results(results, args):
         assert len(this_results) == 1
         this_results = this_results[0]
         res.extend(this_results)
-        assert len(this_results) > 0, f"no results found for index {index}"
+        if len(this_results) == 0:
+            print(f"no results found for index {index}")
         this_results = np.array(this_results)
         this_results[this_results < 0] = 0
         per_prob_res.append(np.mean(this_results))
@@ -47,12 +48,46 @@ def print_results(results, args):
     print(f"Test Case Average (average accuracy over problems) = {np.mean(per_prob_res)}")
     print(f"Strict Accuracy (all test cases passed / total problems) = {np.mean(all_correct)}")
 
+def print_results_old(results, args):
+    res = []
+    per_prob_res = []
+    all_correct = []
+    for index in results:
+       res.extend(results[index])
+       per_prob_res.append(np.mean(results[index]))
+       all_correct.append(np.all(results[index]))
+    tmp_results = res
+    compile_errors = len(tmp_results[tmp_results==-2])
+    runtime_errors = len(tmp_results[tmp_results==-1])
+    failures = len(tmp_results[tmp_results==False])
+    successes = len(tmp_results[tmp_results==True])
+    total_testcases = len(res)
+    if args.debug:
+        print(f"number of compile errors = {compile_errors} avg = {compile_errors / total_testcases }")
+        print(f"number of runtime errors = {runtime_errors} avg = {runtime_errors / total_testcases}")
+        print(f"number of test cases run = {total_testcases}")
+
+    print(f"Test Case Average (average accuracy over problems) = {np.mean(per_prob_res)}")
+    print(f"Strict Accuracy (all test cases passed / total problems) = {np.mean(all_correct)}")
+
 
 def eval_and_save_problems(args):
     with open(args.test_loc, "r") as f:
         problems = sorted(json.load(f))
-
     print(len(problems))
+
+    if args.shard is not None:
+        assert 0 <= args.shard < args.num_shards
+        shard_size = int(math.ceil(len(problems) / args.num_shards))
+        start = shard_size * args.shard
+        end = shard_size * (args.shard + 1)
+        if args.shard == args.num_shards - 1:
+            assert end == len(problems)
+        args.start = start
+        args.end = end
+        print(f"shard {args.shard}: [{args.start}, {args.end})")
+
+
     gpt_codes = {}
     gpt_bleu = {}
     gpt_codebleu = {}
@@ -96,8 +131,8 @@ def eval_and_save_problems(args):
             continue
         prob_path = os.path.join(args.root, problem)
 
-        with open(os.path.join(prob_path, "solutions.json"), "r") as f:
-            sols = json.load(f)
+        # with open(os.path.join(prob_path, "solutions.json"), "r") as f:
+        #     sols = json.load(f)
         
         if not os.path.exists(args.save):
             os.makedirs(args.save)
@@ -167,6 +202,7 @@ def main(args):
         results = eval_and_save_problems(args)
 
     print_results(results, args)
+    print_results_old(results, args)
 
 
 if __name__ == "__main__":
@@ -180,6 +216,8 @@ if __name__ == "__main__":
     parser.add_argument("-r","--root", default="../", type=str, help="where the data is stored.")
     parser.add_argument("-s","--start", default=0, type=int)
     parser.add_argument("-e","--end", default=None, type=int, help="If you want to evaluate a subset of problems specify start and ending index. File with start and ending prefix must exist typically used with batch evaluation.")
+    parser.add_argument("--shard", default=None, type=int)
+    parser.add_argument("--num_shards", default=10, type=int)
     parser.add_argument("-i", "--index", default=0, type=int)
     parser.add_argument("-p", "--print_results", action="store_true", help="If you have already evaluated the results and only want to print them.")
     parser.add_argument("-d", "--debug", action="store_true")
